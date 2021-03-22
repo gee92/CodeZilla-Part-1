a3c2a7efdce65f4203663453583d822402772c48
Geneara Boardley <gcodez@Genearas-MacBook-Air.local>
2021-03-22 19:31:06 -0400

    Notcho

---
+++ class-Project/Nacho-Fibre/NachoFibre.js
@@ -0,0 +1,18 @@
+
+class NachoFibre{
+
+    fibonacci(n){
+        if(n == 0 ){
+            return 0; 
+        }
+        return "I am lost at sea"
+        ;
+    }
+
+    kaleb(n){
+        return -1;
+    }
+
+}
+
+module.exports = NachoFibre;
